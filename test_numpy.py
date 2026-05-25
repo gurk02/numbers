fe0e630d54d2e481a2b74462bacbccd9f159459d
gurk02 <ges_msn@hotmail.com>
2026-05-25 09:44:02 -0700

    feat made top3 function module added test py code for different ways of number processing

---
+++ test_numpy.py
@@ -0,0 +1,15 @@
+# import library
+# import numpy as np
+
+# a1 = np.arange(10)
+# print(a1)
+# print(a1.shape)
+
+
+
+# arr1 = np.array([1, 2, 3])
+# print(arr1)
+
+import numpy as np
+arr1 = np.array([1, 2, 3])
+print(arr1)
---
+++ test_scikit-learn.py
@@ -0,0 +1,63 @@
+# import library
+#
+# common machine learning algorithms - regression, clustering, classifications, decision trees and more
+#
+#
+# exercise 
+# 1) implmenet own machine learning algorithms
+# 2) instead of own use scikit-learn lib
+
+#
+# machine learning 
+#
+
+# traditional programming data & program -> output
+
+# machine learning data & output -> program (derive the model) a set of rules to make predictions
+
+# example - accounting
+# traditional:
+# data collected include sales records, inventory lists, so on..
+# program would take data and calulate profits or loss based on sales records
+# the output is the p/l statement with possible charts..
+#
+
+# example - accounting
+# machine learning:
+# would tkae detailed sales records(which are collectively both the data and output)
+# and use them to derive a set of rules to make predictions.
+# you may use this model to predict the 
+# most popular items that will sell next year
+# or which items will be less popular going forward
+
+
+# what problems will machine learning solve?
+#
+# there are 3 main problem types
+
+# 1) classifacation: is this A or B?
+# 2) Regression: How much or how many?
+# 3) How is this organized?
+
+
+# explore each
+# 
+# classification
+# 1) predicting the winnner of election
+# 2) predicting if a tumor is cancerous
+# 3) classifying the different types of flowers
+#
+
+# regression
+# 1) predicting the sales number for a particular item for next quarter
+# 2) predicting the temperatures for next week
+# 3) predicting the lifespan of a particular model of tire
+
+# clustering
+# 1) which viewers like the same genre of movies
+# 2) which models of hard drives fail in the same way
+#
+
+
+
+
