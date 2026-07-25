6311351cc85a166a498a765ad6f2ff5a170f2ac9
gurk02 <ges_msn@hotmail.com>
2026-07-25 11:20:51 -0700

    3numbers: add calculate_n1_pattern_V2 - modified rule#1, rule#2

--- 3numbers.py
+++ 3numbers.py
@@ -575,6 +575,9 @@ def calculate_n1_ppattern_V2(pattern):
     prev_direction_rate = 0
     delta_direction_rate = 0
     direction_correct = 0
+    direction_draw = 0
+    direction_below_five = 0
+    direction_above_five = 0
     below_five_prec = 0
     above_five_prec = 0
 
@@ -584,25 +587,31 @@ def calculate_n1_ppattern_V2(pattern):
 
     for n in pattern:
 
+        print("draw# ", i_cnt)
+        print("previous")
         print(f"prev_num1 {prev_num1} curr_num1 {curr_num1} n {n}")
 
         # decision rules here
-        print(f"direction_rate {direction_rate}, delta_direction_rate{delta_direction_rate} %")
-        # if delta_direction_rate <= -60:
-        #     # flip direction
-        #     if direction == "low":
-        #         print("go high#0")
-        #         direction="high"
-        #         pred_n1 = get_high(n1_ppoints, 5, 9)
-        #     elif direction == "high":
-        #         print("go low#0")
-        #         direction="low"
-        #         pred_n1 = get_high(n1_ppoints, 0, 5)
-        #     else:
-        #         print("go all range#0")
-        #         direction="all"
-        #         pred_n1 = get_high(n1_ppoints, 0, 9)
 
+        print(f"direction_rate {direction_rate}, delta_direction_rate {delta_direction_rate} %")
+        print(f"previous direction = {direction}")
+        # rule#1 if direction rate very high go in opposite
+        if delta_direction_rate <= -6:
+            # flip direction
+            if direction == "low":
+                print("go high#0")
+                direction="high"
+                pred_n1 = get_high(n1_ppoints, 5, 9)
+            elif direction == "high":
+                print("go low#0")
+                direction="low"
+                pred_n1 = get_high(n1_ppoints, 0, 5)
+            else:
+                print("go all range#0")
+                direction="all"
+                pred_n1 = get_high(n1_ppoints, 0, 9)
+        
+        # rule#2 precent below or above five
         if below_five_prec >= 60:
             print("go high#1")
             direction="high"
@@ -620,7 +629,7 @@ def calculate_n1_ppattern_V2(pattern):
             direction="high"
             pred_n1 = get_high(n1_ppoints, 5, 9)
         else:
-            # 0 point tied flip a coin or random or opposite last direction high or low
+            # # 0 point tied flip a coin or random or opposite last direction high or low
             if direction == "low":
                 # go high
                 print("go high#3")
@@ -628,7 +637,8 @@ def calculate_n1_ppattern_V2(pattern):
                 pred_n1 = get_high(n1_ppoints, 5, 9)
             elif direction == "high":
                 # go low - opposite
-                direction="low#3"
+                print("go low#3")
+                direction="low"
                 pred_n1 = get_high(n1_ppoints, 0, 5)
             else:
                 # undecided go with next in line all range
@@ -640,8 +650,9 @@ def calculate_n1_ppattern_V2(pattern):
         # pred_n1 = get_high(n1_ppoints)
         #pred_n1 = get_high(n1_ppoints)
         # pred_n1 = 4
+                
+        print("predict ", pred_n1)
         print(f"pred_n1 {pred_n1}, below_five {n1_below_five} above_five {n1_above_five} direction {direction}")
-        
         # predict
         if n == pred_n1:
             n1_ppoints[pred_n1] += 1
@@ -651,7 +662,8 @@ def calculate_n1_ppattern_V2(pattern):
             print(f"{n} Match {pred_n1}? N")
             n1_ppoints[pred_n1] -= 1
 
-        print(f"n {n}, prev_n1 {prev_n1[n]}, curr_n1 {curr_n1[n]}", f"\nn1_cnt {n1_cnt}",f"\nn1_ppoints {n1_ppoints}")
+        # print(f"n {n}, prev_n1 {prev_n1[n]}, curr_n1 {curr_n1[n]}", f"\nn1_cnt {n1_cnt}",f"\nn1_ppoints {n1_ppoints}")
+        print(f"\nn1_cnt {n1_cnt}",f"\nn1_ppoints {n1_ppoints}")
 
 
         # calcutaions 
@@ -677,20 +689,33 @@ def calculate_n1_ppattern_V2(pattern):
 
         below_five_prec = int((below_five_cnt / i_cnt) * 100)
         above_five_prec = int((above_five_cnt / i_cnt) * 100)
-        
+
+
+        # if pred_n1 < 5:
+        #     direction_below_five += 1
+        # elif pred_n1 >=5 and pred_n1 <=9:
+        #     direction_above_five += 1
+
         prev_direction_rate = direction_rate
         direction_rate =  int((direction_correct / i_cnt) * 100)
+        
         if direction_rate == 0:
             direction_rate = 1
         if prev_direction_rate == 0:
             prev_direction_rate = 1
+        
         delta_direction_rate = int(( direction_rate / prev_direction_rate ) * 100) - 100
         
         prev_num1 = curr_num1
         curr_num1 = n
 
+        print("report")
         #report
-        print(f"below_five {n1_below_five} above_five {n1_above_five} below_five_cnt{below_five_cnt} below_five_prec{below_five_prec} %, above_five_cnt{above_five_cnt} above_five_prec{above_five_prec} % ,direction_rate {direction_rate} %")
+        print(f"below_five {n1_below_five} above_five {n1_above_five}")
+        print(f"below_five_cnt {below_five_cnt} below_five_prec {below_five_prec} %")
+        print(f"above_five_cnt {above_five_cnt} above_five_prec {above_five_prec} % ")
+        print(f"direction_rate {direction_rate} % direction_correct {direction_correct}")
+        print(f"match_cnt {n1_match_cnt}")
         print("\n")
     
 
